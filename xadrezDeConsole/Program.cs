fd4d5a446b17415e25bfb83a142212f54c6c9be8
jvitormichels <jvitormichels@gmail.com>
2021-01-25 10:53:31 -0300

    Criado a classe PosicaoXadrez que representa a posição segundo a linguagem do tabuleiro real (a-f + 1-8)

--- xadrezDeConsole/Program.cs
+++ xadrezDeConsole/Program.cs
@@ -8,10 +8,14 @@ namespace xadrezDeConsole
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
+
+            /*Tabuleiro tab = new Tabuleiro(8, 8);
             tab.ColocarPeca(new Torre(tab, Cor.Amarela), new Posicao(7,0));
             tab.ColocarPeca(new Rei(tab, Cor.Amarela), new Posicao(0, 7));
-            Tela.ImprimirTabuleiro(tab);
+            Tela.ImprimirTabuleiro(tab);*/
         }
     }
 }
