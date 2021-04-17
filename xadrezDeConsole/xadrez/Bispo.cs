42cad63bf1811563d3064279cfbc649a15952eb5
jvitormichels <jvitormichels@gmail.com>
2021-04-17 17:47:42 -0300

    feat: horse

--- xadrezDeConsole/xadrez/Bispo.cs
+++ xadrezDeConsole/xadrez/Bispo.cs
@@ -1,10 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using tabuleiro;
 
-namespace xadrezDeConsole.xadrez
+namespace xadrez
 {
-    class Bispo
+    class Bispo : Peca
     {
+        public Bispo(Tabuleiro tab, Cor cor) : base(cor, tab)
+        {
+        }
+
+        public bool PodeMover(Posicao pos)
+        {
+            Peca p = Tab.QualPeca(pos);
+            return p == null || p.Cor != this.Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] aux = new bool[Tab.Linhas, Tab.Colunas];
+            Posicao pos = new Posicao(0, 0);
+
+            //Cima - Esquerda
+            pos.DefinirValores(Posicao.linha - 1, Posicao.coluna - 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+                if ((Tab.QualPeca(pos) != null) && (Tab.QualPeca(pos).Cor != this.Cor))
+                {
+                    break;
+                }
+                pos.linha = pos.linha - 1;
+            }
+
+            //Baixo - Direita
+            pos.DefinirValores(Posicao.linha + 1, Posicao.coluna + 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+                if ((Tab.QualPeca(pos) != null) && (Tab.QualPeca(pos).Cor != this.Cor))
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 1;
+            }
+
+            //Cima - Direita
+            pos.DefinirValores(Posicao.linha - 1, Posicao.coluna + 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+                if ((Tab.QualPeca(pos) != null) && (Tab.QualPeca(pos).Cor != this.Cor))
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna + 1;
+            }
+
+            //Baixo - Esquerda
+            pos.DefinirValores(Posicao.linha + 1, Posicao.coluna - 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+                if ((Tab.QualPeca(pos) != null) && (Tab.QualPeca(pos).Cor != this.Cor))
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+            }
+
+            return aux;
+        }
+
+        public override string ToString()
+        {
+            return "B";
+        }
     }
 }
---
+++ xadrezDeConsole/xadrez/Cavalo.cs
@@ -0,0 +1,86 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Cavalo : Peca
+    {
+        public Cavalo(Tabuleiro tab, Cor cor) : base(cor, tab)
+        {
+        }
+
+        public bool PodeMover(Posicao pos)
+        {
+            Peca p = Tab.QualPeca(pos);
+            return p == null || p.Cor != this.Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] aux = new bool[Tab.Linhas, Tab.Colunas];
+            Posicao pos = new Posicao(0, 0);
+
+            //Esquerda Cima
+            pos.DefinirValores(Posicao.linha - 1, Posicao.coluna - 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+            }
+
+            //Esquerda Baixo
+            pos.DefinirValores(Posicao.linha + 1, Posicao.coluna - 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+            }
+
+            //Direita Cima
+            pos.DefinirValores(Posicao.linha - 1, Posicao.coluna + 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+            }
+
+            //Direita Baixo
+            pos.DefinirValores(Posicao.linha + 1, Posicao.coluna + 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+            }
+
+            //Cima Direita
+            pos.DefinirValores(Posicao.linha - 2, Posicao.coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+            }
+
+            //Cima Esquerda
+            pos.DefinirValores(Posicao.linha - 2, Posicao.coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+            }
+
+            //Baixo Direita
+            pos.DefinirValores(Posicao.linha + 2, Posicao.coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+            }
+
+            //Baixo Esquerda
+            pos.DefinirValores(Posicao.linha + 2, Posicao.coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                aux[pos.linha, pos.coluna] = true;
+            }
+
+            return aux;
+        }
+
+        public override string ToString()
+        {
+            return "C";
+        }
+    }
+}
